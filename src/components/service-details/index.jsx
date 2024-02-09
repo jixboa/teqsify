848364cc8ce69d62011089c71c2864c0a628fa4d
teqsify <teqsify.it@gmail.com>
2024-02-09 13:03:55 +0000

    Servie details

--- src/components/service-details/index.jsx
+++ src/components/service-details/index.jsx
@@ -3,7 +3,7 @@ import HeaderTwo from "@/src/layout/headers/header-2";
 import React from "react";
 import ServiceDetailsArea from "./service-details-area";
 import FooterContact from "@/src/layout/footers/footer-contact";
-import FooterThree from "@/src/layout/footers/footer-3";
+import FooterTwo from "@/src/layout/footers/footer-2";
 
 const ServiceDetails = () => {
   return (
@@ -17,7 +17,7 @@ const ServiceDetails = () => {
         <ServiceDetailsArea />
         <FooterContact bg_style={true} />
       </main>
-      <FooterThree />
+      <FooterTwo />
     </>
   );
 };
